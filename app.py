38eb86ffb145bf6baae1b7e7a14173f8d225c665
Adam <adamshamsudeen@gmail.com>
2019-02-20 15:17:05 +0530

    added lucky animal

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +9,11 @@ def hello_world():
 @app.route('/animals')
 def page():
 	animals = ['cat', 'cow', 'horse', 'dog']
-	return render_template('animals.html',animals=animals, title="Animals")
+	lucky_animal = random.choice(animals)
+	return render_template('animals.html',
+					animals = animals,
+					title = "Animals",
+					lucky_animal = lucky_animal)
 
 
 if __name__ == '__main__':
